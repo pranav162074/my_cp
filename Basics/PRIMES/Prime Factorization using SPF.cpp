3bf1b107ded75c38d07101afcd36795b9223bce1
Pranav Reddy <70427156+pranav162074@users.noreply.github.com>
2026-06-26 19:04:42 +0530

    Implement SPF function for prime factorization
    Added a function to compute the smallest prime factor (SPF) for each number up to n.

--- Basics/PRIMES/Prime Factorization using SPF.cpp
+++ Basics/PRIMES/Prime Factorization using SPF.cpp
@@ -15,6 +15,24 @@ Factors:
 2 2 3 7
 */
 
+vector<int> SPF(int n) {
+    vector<int> spf(n + 1);
+
+    for (int i = 0; i <= n; i++)
+        spf[i] = i;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (spf[i] == i) {
+            for (int j = i * i; j <= n; j += i) {
+                if (spf[j] == j)
+                    spf[j] = i;
+            }
+        }
+    }
+
+    return spf;
+}
+
 vector<int> primeFactors(int n, vector<int>& spf) {
     vector<int> factors;
 
