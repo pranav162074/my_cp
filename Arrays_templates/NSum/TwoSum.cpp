9743b8464ce3741aef9ced6f6055ca9b1840b23f
Pranav Reddy <70427156+pranav162074@users.noreply.github.com>
2026-06-26 19:49:56 +0530

    Implement TwoSum solution in TwoSum.cpp

---
+++ Arrays_templates/NSum/TwoSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        for (int i=0; i<n; i++) {
+            int need = target-nums[i];
+            if (mp.find(need) != mp.end()) {
+                return {i, mp[need]};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};//never reaches here
+    }
+};
